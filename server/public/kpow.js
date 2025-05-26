d4d2ecc110ca24bbbd3e76eec77b7473eae8f7b0
Sultan Iman <sultan.imanhodjaev@gmail.com>
2025-05-26 22:26:45 +0200

    implement color theme switch

--- server/public/kpow.js
+++ server/public/kpow.js
@@ -1,35 +1,55 @@
 (function () {
   const mQuery = matchMedia("(prefers-color-scheme: dark)");
+  const themeKey = "theme";
+  const themeAttr = "data-theme";
+  const dark = "dark";
+  const light = "light";
 
   // Track color theme change
   mQuery.onchange = ({ matches }) => {
     if (matches) {
-      localStorage.setItem("theme", "dark");
-      document.documentElement.setAttribute("data-theme", "dark");
+      setColorTheme(dark)
     } else {
-      localStorage.setItem("theme", "light");
-      document.documentElement.setAttribute("data-theme", "light");
+      setColorTheme(light)
     }
   };
 
   const getColorScheme = () => {
     // If media query doesn't match then light is default
-    let colorScheme = "light";
-    if ("theme" in localStorage) {
-      return localStorage.getItem("theme");
+    if (themeKey in localStorage) {
+      return localStorage.getItem(themeKey);
     } else {
       if (mQuery.matches) {
-        colorScheme = "dark";
+        return dark;
       }
-
-      localStorage.setItem("theme", colorScheme);
     }
 
-    return colorScheme;
+    return light;
   };
+
+  const setColorTheme = (colorMode) => {
+    localStorage.setItem(themeKey, colorMode);
+    document.documentElement.setAttribute(themeAttr, colorMode);
+    document.querySelectorAll(".mode-icon").forEach((el) => {
+      const colorTheme = el.dataset.colorTheme;
+      if (colorTheme !== colorMode) {
+        el.removeAttribute("data-checked");
+      } else {
+        el.setAttribute("data-checked", "true");
+      }
+    })
+  }
 
   // Detect color theme and set the relevant value
   window.onload = () => {
-    document.documentElement.setAttribute("data-theme", getColorScheme());
+    setColorTheme(getColorScheme());
+
+    document.querySelectorAll(".mode-icon").forEach((el) => {
+      el.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        const targetEl = evt.target.closest("span");
+        setColorTheme(targetEl.dataset.colorTheme);
+      })
+    })
   };
 }());
